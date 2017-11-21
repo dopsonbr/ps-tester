6ddef8cc952d8faf1d80d87ed25e71a2f6374bab
Brian Dopson <brian_dopson1@homedepot.com>
2017-11-20 21:08:05 -0500

    update hostnames

--- load_test.sh
+++ load_test.sh
@@ -3,12 +3,12 @@
 
 curl "http://client/router/health" || echo 'client failed!'
 curl "http://router/metrics" || echo 'router failed!'
-curl "http://web_service/health" || echo 'web_service failed!'
+curl "http://web-service/health" || echo 'web_service failed!'
 
 echo '********************************************************'
 echo 'starting tests'
 echo '********************************************************'
 
-curl "http://client:80/router/ps-sample/hello" 
+curl "http://client:80/router/service-a/hello" 
 curl "http://router/ps-sample/hello" 
-#ab -c 1 -t 10 "http://router/ps-sample/hello"
+ab -c 1 -t 10 "http://router/service-a/hello"
